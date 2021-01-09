404eb773306d50cd4175d2676828cdaeb6088be8
eek <leonardolp@gmail.com>
2021-01-08 21:25:49 -0300

    Add arquivos

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
+
+import "./styles.css";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchRepo() {
+      const response = await api.get("/repositories");
+
+      setData(response.data);
+    }
+
+    fetchRepo();
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post("/repositories", {
+      title: "Gobarber Web",
+      url: "https://github.com/leoeek/gobarber-web-react",
+      techs: ["JavaScript", "ReactJS"],
+    });
+
+    setData([...data, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`);
+
+    setData(data.filter((repo) => repo.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {data.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
